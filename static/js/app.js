32a929a8e0c8de80e0386eb2db9d565a0023322e
Ssimoes48 <pbear48@aol.com>
2020-12-27 11:07:42 -0500

    added charts and bubble

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,7 @@ function init() {
     d3.json("samples.json").then((response) => {
         var names = response.names;
 
-        console.log(names);
+        console.log(response);
 
         names.forEach(name => {
 
@@ -21,6 +21,7 @@ function init() {
         });
 
         buildMetadata(names[0]);
+        buildCharts(names[0]);
     });
 }
 
@@ -41,55 +42,56 @@ function buildMetadata(sample) {
                 .text(`${key}: ${value}`);
         });
     });
-};
+}
 // Parse and filter the data to get the sample's metadata
 // Specify the location of the metadata and update it
 
 // Define a function that will create charts for given sample
 function buildCharts(sample) {
-    // d3.json("samples.json").then((response) => {
-    //     var samples = response.samples;
-    //     var sampleData = samples.filter(meta => sample.id == sample)[0];
-    // Read the json data
+    d3.json("samples.json").then((response) => {
+        var samples = response.samples;
+        var sampleData = samples.filter(button => button.id == sample)[0];
+        // Read the json data
+        console.log(sampleData);
 
-    // Parse and filter the data to get the sample's OTU data
+        // Parse and filter the data to get the sample's OTU data
 
-    // Pay attention to what data is required for each chart
+        // Pay attention to what data is required for each chart
 
-    // Create bar chart in correct location
-    // var data = [{
-    //     type: 'bar',
-    //     x: [20, 14, 23],
-    //     y: ['giraffes', 'orangutans', 'monkeys'],
-    //     orientation: 'h'
-    //   }];
-      
-    //   Plotly.newPlot('myDiv', data);
+        // Create bar chart in correct location
+        var data = [{
+            type: 'bar',
+            x: sampleData.sample_values.slice(0, 10).reverse(),
+            y: sampleData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+            text: sampleData.otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+        }];
 
-    // Create bubble chart in correct location
-    // var trace1 = {
-    //     x: [1, 2, 3, 4],
-    //     y: [10, 11, 12, 13],
-    //     mode: 'markers',
-    //     marker: {
-    //       color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-    //       opacity: [1, 0.8, 0.6, 0.4],
-    //       size: [40, 60, 80, 100]
-    //     }
-    //   };
-      
-    //   var data = [trace1];
-      
-    //   var layout = {
-    //     title: 'Marker Size and Color',
-    //     showlegend: false,
-    //     height: 600,
-    //     width: 600
-    //   };
-      
-    //   Plotly.newPlot('myDiv', data, layout);
+          Plotly.newPlot('bar', data);
 
+        // Create bubble chart in correct location
+        var trace1 = {
+            x: sampleData.otu_ids,
+            y: sampleData.sample_values,
+            mode: 'markers',
+            text: sampleData.otu_labels,
+            marker: {
+              color: sampleData.otu_ids,
+              size: sampleData.sample_values,
+              colorscale: 'Picnic'
+            }
+          };
 
+          var data2 = [trace1];
+
+          var layout = {
+            title: 'Belly Button',
+            showlegend: false,
+          };
+
+          Plotly.newPlot('bubble', data2, layout);
+
+    });
 }
 
 function optionChanged(newSample) {
